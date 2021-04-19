4adb48c4847ca6ad2db859735fcc447bd3397ee3
hannah-111 <hannah92.hailu@gmail.com>
2021-04-18 20:54:10 -0400

    hihihi

---
+++ Demo2.1/src/Practice/Javadoc.java
@@ -0,0 +1,11 @@
+package Practice;
+
+public class Javadoc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		System.out.println("Hello World from Hannah");
+	}
+
+}
