58c0618b8a8586313137df5d87b73983bdb6ffe0
hannah-111 <hannah92.hailu@gmail.com>
2021-04-18 20:56:27 -0400

    commit

---
+++ Demo3.5/src/Practice/Javadoc.java
@@ -0,0 +1,12 @@
+package Practice;
+
+public class Javadoc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		System.out.println("Hello World");
+		System.out.println("selam naw?");
+		System.out.println("I hope everyone is doing well.");
+	}
+}
